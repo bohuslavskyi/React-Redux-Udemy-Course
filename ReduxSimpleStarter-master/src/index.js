ef2e8c9cb2b942186feef4163fdf066d8068d66e
Idan <bohuslavskyi@gmail.com>
2018-01-03 15:31:15 +0200

    Signed up for the Youtube API, create new component 'search_bar', exploited it and then rendered it inside the 'App'

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search_bar'
+
+
+// YouTube API_KEY
+const API_KEY = 'AIzaSyDFSqRooQ5NPJYdVoKGgaoG8WegEdBAkj4';
 
 
 
 //Create a new component. this component should produce some HTML
 const App = () => {
-    return <div>Hi!</div>
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
 }
 
 //Take this component's generated HTML and put it on the page (in the DOM)
